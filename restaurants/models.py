3d91e991e45c1c6399015d828065d835b9fdd618
SaadAlkandari <mr.s.alkandari@gmail.com>
2022-04-13 02:30:53 +0300

    fixed

--- restaurants/models.py
+++ restaurants/models.py
@@ -5,5 +5,5 @@ from django.db import models
 class Restaurant(models.Model):
     name= models.CharField(max_length=120)
     description= models.TextField()
-    opening_time= models.DateTimeField(auto_now=True)
-    closing_time= models.DateTimeField(auto_now=True)
+    opening_time= models.TimeField(auto_now=False)
+    closing_time= models.TimeField(auto_now=False)
