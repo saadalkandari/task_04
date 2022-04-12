238ee2032dc5dd53e3c27182a06b245960ac9ef4
SaadAlkandari <mr.s.alkandari@gmail.com>
2022-04-13 02:11:18 +0300

    donezz

--- restaurants/models.py
+++ restaurants/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+class Restaurant(models.Model):
+    name= models.CharField(max_length=120)
+    description= models.TextField()
+    opening_time= models.DateTimeField(auto_now=True)
+    closing_time= models.DateTimeField(auto_now=True)
